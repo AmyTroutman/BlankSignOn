a78ad689dcd86e4f525bb4271af60b93cacc4506
AmyTroutman <amyrtroutman@gmail.com>
2020-07-02 20:52:49 -0500

    register works.

--- BlankSignOn/src/app/login/login.component.ts
+++ BlankSignOn/src/app/login/login.component.ts
@@ -14,7 +14,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit(): void {
     this.LoginForm = new FormGroup({
-      username: new FormControl(null, [Validators.required]),
+      username: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required])
     });
   }
--- BlankSignOn/src/app/registration/registration.component.ts
+++ BlankSignOn/src/app/registration/registration.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-registration',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  RegisterForm: FormGroup;
+  constructor(private routerService: Router) { }
 
   ngOnInit(): void {
+    this.RegisterForm = new FormGroup({
+      username: new FormControl(null, [Validators.required, Validators.email]),
+      password: new FormControl(null, [Validators.required]),
+      passwordConfirm: new FormControl(null, [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    console.log(this.RegisterForm);
+    this.routerService.navigate(['/login']);
   }
 
 }
