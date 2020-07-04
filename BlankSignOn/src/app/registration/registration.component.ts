cced42bb05de3e367d67550ed03d70595395939d
AmyTroutman <amyrtroutman@gmail.com>
2020-07-04 16:17:29 -0500

    added passwordValidator. Cleaned up validition.

--- BlankSignOn/src/app/registration/registration.component.ts
+++ BlankSignOn/src/app/registration/registration.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators, FormArray } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormArray, AbstractControl, ValidationErrors } from '@angular/forms';
 import { Router } from '@angular/router';
 
 
@@ -11,19 +11,45 @@ import { Router } from '@angular/router';
 export class RegistrationComponent implements OnInit {
 
   RegisterForm: FormGroup;
-  constructor(private routerService: Router) { }
-
-  ngOnInit(): void {
+  constructor(private routerService: Router) {
     this.RegisterForm = new FormGroup({
       username: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required]),
       passwordConfirm: new FormControl(null, [Validators.required])
+    },
+    {
+      validators: this.passwordValidator
     });
+  }
+
+  get usernameInvalid() {
+    return !this.RegisterForm.controls.username.valid && this.RegisterForm.controls.username.touched;
+  }
+  get passwordInvalid() {
+    return !this.RegisterForm.controls.password.valid && this.RegisterForm.controls.password.touched;
+  }
+  get passwordConfirmInvalid() {
+    return !this.RegisterForm.controls.passwordConfirm.valid && this.RegisterForm.controls.passwordConfirm.touched;
+  }
+  get passwordError(): boolean {
+    return this.RegisterForm.hasError('passwordError');
+  }
+  ngOnInit(): void {
   }
 
   onSubmit() {
     console.log(this.RegisterForm);
     this.routerService.navigate(['/login']);
+  }
+
+  passwordValidator(control: AbstractControl): ValidationErrors {
+    const password = control.get('password');
+    const confirm = control.get('passwordConfirm');
+
+    if (confirm.value !== '' && password.value !== confirm.value) {
+      return {passwordError: true};
+    }
+    return null;
   }
 
 }
