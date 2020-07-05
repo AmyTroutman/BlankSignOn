f2fd17927923bb3aa475d8e2bfe72684c4707b22
AmyTroutman <amyrtroutman@gmail.com>
2020-07-05 12:49:41 -0500

    authGuard set

--- BlankSignOn/src/app/app-routing.module.ts
+++ BlankSignOn/src/app/app-routing.module.ts
@@ -3,12 +3,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { AuthGuard } from './auth/auth.guard';
 
 
 const routes: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegistrationComponent},
-  {path: 'userprofile', component: UserProfileComponent},
+  {path: 'userprofile', component: UserProfileComponent, canActivate: [AuthGuard]},
   {path: '**', redirectTo: 'login', pathMatch: 'full'}
 ];
 
